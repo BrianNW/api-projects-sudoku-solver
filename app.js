93cfd93697de8a0e0bc1312c131e7dad55ea9d5c
Brian <briantechdom@gmail.com>
2022-08-09 07:20:34 -0700

    Update dotenv notes

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+//API keys and route are in .env file. Generate new .env file with 
+//const API_KEY = ''
+//const API_HOST = ''
+//Then install npm install dotenv --save
+//And load them into the variable below
+const dotenv = require('dotenv').config()
+
 const puzzleBoard = document.getElementById('puzzle')
 const solveButton = document.getElementById('solve-button')
 
